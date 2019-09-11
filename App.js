ea4c0af6ebe4be6136b310fdb9d24a10876455f2
PradeepKumarBbiswal <47264707+PradeepKumarBbiswal@users.noreply.github.com>
2019-09-11 15:00:00 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { AppRegistry,View } from 'react-native';
+import Basketball from './Basketball';
+const App = () => (
+  
+  <Basketball />
+);
+
+export default App;
+
+AppRegistry.registerComponent('Basketball', () => App);
